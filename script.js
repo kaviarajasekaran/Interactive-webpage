c60783e49bbac3879e067c0efe64e2c2ad2fc884
unknown <23csd21@CSELAB.IN>
2026-02-19 14:51:54 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function changePage() {
+    document.getElementById("main-title").innerHTML = "You Clicked the Button!";
+    document.getElementById("description").innerHTML = "The page has changed.";
+    document.body.style.backgroundColor = "lightgreen";
+}
